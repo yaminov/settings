73540f4a97455158cd66c281f4c3b6177083e218
kamil <k.r.yaminov@gmail.com>
2017-04-03 13:49:23 +0700

    add scrips to setup virtual eth0 for quartus licensing

--- nix/etc/eth-alias.sh
+++ nix/etc/eth-alias.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 modprobe dummy
-ifconfig dummy0 hw ether 00:26:18:9b:ab:3c
-#ifup dummy0
+ifconfig dummy0 hw ether 00:01:02:03:04:05
 ip link set name eth0 dev dummy0
+ifconfig eth0 up
 
