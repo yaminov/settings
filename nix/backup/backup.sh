cc1aeaae16a1a17367fda88f6198c10cd2aaf9f9
kamil <k.r.yaminov@gmail.com>
2020-06-10 12:01:58 +0700

    new backup settings and scripts

--- nix/backup/backup.sh
+++ nix/backup/backup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-rsync -avzh --delete --progress /home/kamil/pro/ /mnt/arc-backup/pro/
-date > /mnt/arc-backup/pro/last-backup
+#create local backup tarball
+tar --exclude=build --exclude=bin --exclude='*.raw' -zcvf /opt/backup/pro/pro.tar.gz /mnt/nvme/pro/
+
+#sync with remote backup server
+rsync -avzh --delete /opt/backup/pro /mnt/esafeline/backup
+
+#sync with cloud
+rclone sync /opt/backup/pro gdrive:backup
+
